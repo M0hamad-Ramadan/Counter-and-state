984379bc23fefd9bb42c9cfa9f6cd888a0bd9b7c
mohammed <moaboelyazied@gmail.com>
2024-03-16 04:49:25 +0200

    state-and-add-5-on-click

---
+++ test.js
@@ -0,0 +1,18 @@
+import { useState } from "react";
+
+const MyForm = () => {
+  const [number, setNewState] = useState(0);
+
+  function addFive() {
+    setNewState(number + 5);
+  }
+
+  return (
+    <>
+      <button onClick={addFive}>+5</button>
+      <h3>{number}</h3>
+    </>
+  );
+};
+
+export default MyForm;
